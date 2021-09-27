127d7ddc2ad6629ad3b89ebf11a8cb031e0f7311
psilons <cuirick@gmail.com>
2021-09-26 23:51:43 -0400

    type alias

--- build.sbt
+++ build.sbt
@@ -11,3 +11,4 @@ scalacOptions ++= Seq("-deprecation", "-feature")
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
 libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.6"
+libraryDependencies += "com.chuusai" %% "shapeless" % "2.3.7"
--- src/main/scala/org/mytest/scala/walkthrough/s9_TypeClasses/TypeClass.scala
+++ src/main/scala/org/mytest/scala/walkthrough/s9_TypeClasses/TypeClass.scala
@@ -2,14 +2,43 @@ package org.mytest.scala.walkthrough.s9_TypeClasses
 
 // https://www.baeldung.com/scala/type-classes
 // https://docs.scala-lang.org/scala3/book/types-type-classes.html
-class TypeClass
+object TypeClass extends App
 {
+    // This is very useful for readable code, otherwise we have to use variable name
+    // to make it readable, such as userId2user
+    // https://www.vlachjosef.com/tagged-types-introduction/
+    // https://github.com/milessabin/shapeless
+    trait FirstNameTag
+    trait LastNameTag
+    trait AgeTag
 
+    import shapeless.tag.@@
+    import shapeless.tag
+
+    type FirstName = String @@ FirstNameTag
+    type LastName = String @@ LastNameTag
+    type Age = Int @@ AgeTag
+
+    case class Person(fn: FirstName, ln: LastName, age: Age)
+
+
+    // this is not working since types are not matched
+    //println(Person("Joe", "Smith", 15))
+    val fn: FirstName = tag[FirstNameTag][String]("Joe")
+    val ln: LastName = tag[LastNameTag][String]("Smith")
+    val age: Age = tag[AgeTag][Int](20)
+    println(Person(fn, ln, age))
+    // This is tedious, so Scala3 introduce opaque types, PS3E page 289
+
+    // this is not working, good guard on types, though they are all strings.
+    // println(Person(ln, fn, age))
 }
 
 // PS3E Page 101, abstract type member
 
 
-// This is very useful for readable code, otherwise we have to use variable name
-// to make it readable, such as userId2user
-// https://www.vlachjosef.com/tagged-types-introduction/
+
+
+
+
+
